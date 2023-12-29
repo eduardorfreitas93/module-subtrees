9c584e03e093d26af9ac38eeae80c97cbcab53f8
eduardorfreitas93 <duduramos1@gmail.com>
2023-12-29 09:26:04 -0300

    voltando

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>
-          module subtree standalone teste subtrees teste
+          module voltando
         </h1>
       </header>
     </div>
