7c872521110b83d726feba1c8cc6e9c39b55d053
eduardorfreitas93 <duduramos1@gmail.com>
2023-12-29 09:24:51 -0300

    atualizando o subtree

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>
-          module standalone teste subtrees
+          module subtree standalone teste subtrees teste
         </h1>
       </header>
     </div>
